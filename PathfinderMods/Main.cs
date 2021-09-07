dd259cf74e2734a7f83d7140f9eac1ace64a9bb9
Casey Bailey <soniczentropy@gmail.com>
2021-09-07 15:36:05 -0500

    Initial working version, has single example mod that removes encumberance

---
+++ PathfinderMods/Main.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using UnityEngine;
+using UnityModManagerNet;
+using UnityEngine.UI;
+using HarmonyLib;
+
+namespace PathfinderMods
+{
+    static class Main
+    {
+        public static bool Enabled;
+        public static Settings Settings;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Settings = Settings.Load<Settings>(modEntry);
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            return true;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Enabled = value;
+            return true;
+        }
+
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("MyFloatOption", GUILayout.ExpandWidth(false));
+            GUILayout.Space(10);
+            Settings.MyFloatOption = GUILayout.HorizontalSlider(Settings.MyFloatOption, 1f, 10f, GUILayout.Width(300f));
+            GUILayout.Label($" {Settings.MyFloatOption:p0}", GUILayout.ExpandWidth(false));
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("MyBoolOption", GUILayout.ExpandWidth(false));
+            GUILayout.Space(10);
+            Settings.MyBoolOption = GUILayout.Toggle(Settings.MyBoolOption, $" {Settings.MyBoolOption}", GUILayout.ExpandWidth(false));
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("MyTextOption", GUILayout.ExpandWidth(false));
+            GUILayout.Space(10);
+            Settings.MyTextOption = GUILayout.TextField(Settings.MyTextOption, GUILayout.Width(300f));
+            GUILayout.EndHorizontal();
+        }
+
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            Settings.Save(modEntry);
+        }
+    }
+}
